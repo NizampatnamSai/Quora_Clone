3f0223ed698f71fe08a38b6785b1a8988e8de008
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-02-01 12:29:32 +0530

    userPhoto change

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -104,7 +104,7 @@ const Navbar=()=>{
               src={
                 user.photo
                   ? user.photo
-                  : "https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573"
+                  : "https://previews.123rf.com/images/andrewrybalko/andrewrybalko1904/andrewrybalko190400270/122040116-anonymous-man-with-question-mark-male-person-not-identified-by-name-unknown-faceless-user-incognito-.jpg"
               }
             />
             <p>{user.disPlayName ? user.disPlayName : user.email} asked</p>
